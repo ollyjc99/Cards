5c4e289e540d543ad59a2cb0f0caa554bff637d4
ollyjc99 <44035033+ollyjc99@users.noreply.github.com>
2020-08-04 20:11:32 +0100

    finished

--- pairs.py
+++ pairs.py
@@ -18,11 +18,18 @@ def setup(win):
     y_bord = round((y_spacing*.90) / 2)
     y_btwn = round((y_spacing*.10) / 2)
 
-    # return card_width, card_height, x_bord, y_bord, x_btwn, y_btwn
     return card_width, card_height, [[[x, y] for x in range(x_bord, width-x_bord, card_width+x_btwn)] for y in range(round(y_bord*1.75), height-round(y_bord*.25), card_height+y_btwn)]
 
-def print_title():
-    oass
+
+def print_score(win, win_width, win_height, font, count):
+
+    pair_count = font.render(str(count), True, (255, 255, 255), (75,125,75))
+
+    count_rect = pair_count.get_rect()
+    count_rect.center = (round(win_width * .96), round(win_height * .05))
+
+    win.blit(pair_count, count_rect)
+
 
 
 def flip(card):
@@ -42,13 +49,13 @@ def match(first, second):
 def pairs(win_width, win_height, base, deck):
     win = pygame.display.set_mode((win_width, win_height))
     pygame.display.set_caption('Pairs')
-    win.fill((75,125,75))
+    background_colour = (75,125,75)
+    win.fill(background_colour)
 
     font = pygame.font.Font('static/font/Arial.ttf', 32)
     count = 0
     title = font.render('Pairs', True, (255,255,255))
     score = font.render('Score', True, (255,255,255))
-    count = font.render(str(count), True, (255, 255, 255))
 
     title_rect = title.get_rect()
     title_rect.center = (round(win_width // 2), round(win_height * .20))
@@ -56,12 +63,10 @@ def pairs(win_width, win_height, base, deck):
     score_rect = score.get_rect()
     score_rect.center = (round(win_width * .90), round(win_height * .05))
 
-    count_rect = count.get_rect()
-    count_rect.center = (round(win_width * .95), round(win_height * .05))
-
     win.blit(title, title_rect)
     win.blit(score, score_rect)
-    win.blit(count, count_rect)
+
+    print_score(win, win_width, win_height, font, count)
 
     card_width, card_height, grid = setup(win)
     deck = [base(win, card_width, card_height, suit, face) for face, suit in random.sample(deck, 52)]
@@ -92,6 +97,8 @@ def pairs(win_width, win_height, base, deck):
                                 flip(card)
                                 second = card
                                 if match(first, second):
+                                    count+=1
+                                    print_score(win, win_width, win_height, font, count)
                                     first, second = None, None
                                 else:
                                     pygame.time.wait(1000)
