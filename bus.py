62cf93efc87f883c15c1975b34e55146bd47a68a
Oliver Carter <oliverc101@hotmail.com>
2020-09-10 00:15:18 +0100

    fixed the bus to work with new deck system

--- bus.py
+++ bus.py
@@ -14,44 +14,44 @@ def setup(win, bus_len):
 
     y = round((h // 2) - card_height / 2)
 
-    grid = [[x,y] for x in range(x_bord, w-x_bord, card_width+x_btwn)]
+    grid = [[x, y] for x in range(x_bord, w-x_bord, card_width+x_btwn)]
 
     return card_width, card_height, grid
 
 
 def flip(card):
     card.flipped = not card.flipped
-    card.im = card.flip()
-    card.draw((card.x, card.y))
-    pygame.display.update()
+    card.image = card.flip()
 
 
-def bus(win_width, win_height, base, deck, bus_len, clock):
+def bus(win_width, win_height, deck, bus_len, clock):
     win = pygame.display.set_mode((win_width, win_height))
     pygame.display.set_caption('The Bus')
     background_colour = (75, 125, 75)
-    win.fill(background_colour)
-
     card_width, card_height, grid = setup(win, bus_len)
-    deck = [base(win, card_width, card_height, suit, face) for face, suit in random.sample(deck, bus_len)]
-    p_deck = iter(deck)
+    hand = pygame.sprite.Group()
+    p_deck = iter(deck.cards)
 
     for row in grid:
         print(row)
         card = next(p_deck)
-        card.x, card.y = row
-        card.draw(row)
+        card.rect.topleft = row
+        hand.add(card)
 
     running = True
     while running:
-        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                for card in deck:
-                    if card.im.get_rect(x=card.x, y=card.y).collidepoint(event.pos):
+                for card in deck.cards:
+                    if card.rect.collidepoint(event.pos):
                         flip(card)
+
+        win.fill((75, 125, 75))
+        hand.draw(win)
         pygame.display.update()
+        clock.tick(60)
+
 
